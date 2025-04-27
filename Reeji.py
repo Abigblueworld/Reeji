bb4caadf3a48e5cfa1fc1196b32e24c7d7d58197
BlueWorld/Gruxpin <152799656+Abigblueworld@users.noreply.github.com>
2025-04-26 21:24:27 -0400

    Create Reeji.py
    in Memory saving testing mode

---
+++ Reeji.py
@@ -0,0 +1,9 @@
+//Reeji works using Python
+Termemorytesting = ["null"]
+print ("Test Reeji the system")
+while (0 == 0):
+  Firsttest = input("Input anything so it'll be saved to memory")
+  if ("null" in Termemorytesting):
+    Termemorytesting.remove(Sooos[0])
+  Firsttest.append(Firsttest)
+  print ("Is it saved?: " + Termemorytesting)
