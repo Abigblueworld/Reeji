d315763ecc4015e4dd1e1f69b696eb8bceed38fe
BlueWorld/Gruxpin <152799656+Abigblueworld@users.noreply.github.com>
2025-04-26 21:29:37 -0400

    Update Reeji.py
    memory testing

--- Reeji.py
+++ Reeji.py
@@ -1,9 +1,10 @@
-//Reeji works using Python
+//It is just a memory test
+
 Termemorytesting = ["null"]
 print ("Test Reeji the system")
 while (0 == 0):
-  Firsttest = input("Input anything so it'll be saved to memory")
+  Firsttest = str(input("Input anything so it'll be saved to memory"))
   if ("null" in Termemorytesting):
-    Termemorytesting.remove(Sooos[0])
-  Firsttest.append(Firsttest)
-  print ("Is it saved?: " + Termemorytesting)
+    Termemorytesting.remove(Termemorytesting[0])
+  Termemorytesting.append(Firsttest)
+  print (Termemorytesting)
